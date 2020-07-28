772d8ebfcbf3b6f57295a255eb63ac540e6453e8
YaelLifshitz <64844458+YaelLifshitz@users.noreply.github.com>
2020-07-28 14:07:44 +0300

    begin of summry of c++

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <cstdlib>
 #include <string>
 
-#include "hash.h"
+
 
 using namespace std;
 
-int main() {
-	cout << "Welcome to my Hash Table implementation\n";
-	//std::cin.get();
-	return 0;
-}
+   
+
+    int main() {
+        // summery of C++
+    }
